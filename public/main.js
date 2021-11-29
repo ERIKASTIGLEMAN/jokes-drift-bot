34f08468562af9afae3db0c34db687e773d4b896
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-11-28 21:29:54 -0500

    ngork AHHHHH!

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 const getBtn = () => {
   return document.getElementById('btn')
 }
-
+var conversationId = ''
+var mulberry = ''
 const getData = () => {
   axios
     .get('https://icanhazdadjoke.com/', {
@@ -10,9 +11,64 @@ const getData = () => {
       },
     })
     .then(response => {
-      document.querySelector('#dadJokes').textContent = response.data.joke
+      mulberry = response.data.joke
+      console.log(mulberry)
+      //return mulberry
     })
+    .then(receiveJoke)
 }
+const receiveJoke = function (message) {
+  let request_arg = {
+    method: 'POST',
+    url: `https://driftapi.com/v1/conversations/${conversationId}/messages`,
+    json: true,
+    headers: {
+      'User-Agent': 'request',
+    },
+    data: {
+      messages: [
+        {
+          conversationId: conversationId,
+          body: mulberry,
+        },
+      ],
+    },
+  }
+  return Promise.resolve(request_arg)
+  /*.then(() => {
+    console.log(mulberry)
+    return {
+      type: 'chat',
+     ,
+    }
+  })*/
+}
+
+/*const receiveJoke = () => {
+  console.log('amanda is tired')
+  let XHR = new XMLHttpRequest()
+  XHR.addEventListener('load', function (event) {
+    console.log('Success')
+  })
+  axios
+    .post(`https://driftapi.com/conversations/${conversationId}/messages`, {
+      type: 'mulberry',
+      body: mulberry,
+    })
+    .then(response => {
+      return response()
+    })
+    .then({
+      data: {
+        messages: [
+          {
+            conversationId: conversationId,
+            body: mulberry,
+          },
+        ],
+      },
+    })
+}*/
 
 function main() {
   if (document.querySelector('#btn')) {
@@ -22,3 +78,11 @@ function main() {
 
 //
 document.addEventListener('DOMContentLoaded', main)
+//drift.on('startConversation', test)
+drift.on('message:sent', getData)
+
+drift.on('startConversation', function (data) {
+  conversationId = data.conversationId
+})
+
+//drift.on('message:sent', receiveJoke)
